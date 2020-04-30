ee8f7ecdb91d304b73cce6a9e5fb5aacc6ec7eb7
【momokan928】 <kan.kan19980928@gmail.com>
2020-04-30 20:23:20 +0900

    Ruby on rails　フォームの送信

---
+++ app/controllers/hero_controller.rb
@@ -0,0 +1,20 @@
+class HeroController < ApplicationController
+
+  def index
+    if request.post? then
+      @title = 'Result'
+      if params['s1'] then
+        @msg = 'you selected:'
+        for val in params['s1']
+          @msg += val + ' '
+        end
+      else
+        @msg = 'not selected'
+      end
+    else
+      @title = 'Index'
+      @msg = 'Select radio button...'
+    end
+  end #indexの閉じタグ
+  
+end
